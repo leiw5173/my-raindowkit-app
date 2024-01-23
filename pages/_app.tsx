66a09578007da5f5481544a8375d3e1218dd9c39
Sam <73590328+leiw5173@users.noreply.github.com>
2024-01-23 11:32:12 +0800

    add NeoX testnet entry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,51 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
-  zora,
-} from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+  getDefaultWallets,
+  RainbowKitProvider,
+  Chain,
+} from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { goerli, mainnet, polygon, sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const neox: Chain = {
+  id: 12227329,
+  name: "NeoX",
+  network: "testnet",
+  iconUrl: "/NEO_512_512.png",
+  iconBackground: "#fff",
+  nativeCurrency: {
+    decimals: 18,
+    name: "GAS",
+    symbol: "GAS",
+  },
+  rpcUrls: {
+    public: { http: ["https://neoxseed1.ngd.network:444/"] },
+    default: { http: ["https://neoxseed1.ngd.network:444/"] },
+  },
+  blockExplorers: {
+    default: { name: "NeoXScan", url: "https://xt1scan.ngd.network/" },
+    etherscan: { name: "NeoXScan", url: "https://xt1scan.ngd.network/" },
+  },
+  testnet: true,
+};
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     mainnet,
     polygon,
-    optimism,
-    arbitrum,
-    base,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+      ? [goerli, sepolia, neox]
+      : []),
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
   chains,
 });
 
